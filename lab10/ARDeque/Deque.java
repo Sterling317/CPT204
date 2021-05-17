3386a1d0f70b98bbc4b264df8050783e198ad46a
Sterling317 <74354283+Sterling317@users.noreply.github.com>
2021-05-17 22:51:08 +0800

    Add files via upload

---
+++ lab10/ARDeque/Deque.java
@@ -0,0 +1,14 @@
+package lab10.ARDeque;// LAB EXERCISE 10.1  DEQUE INTERFACE
+
+// Complete the interface for Deque
+// to be implemented by ARDeque
+
+public interface Deque<T> {
+	public int size();
+	public void addLast(T iteem);
+	public void printDeque();
+    public T get(int index);
+    public void addFirst(T item);
+    public T delFirst();
+    public T delLast();
+}
